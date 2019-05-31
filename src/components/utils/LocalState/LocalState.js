637f8c382ba7c6e3e70399712f2d0f79a98aedf2
gaurangrshah <gaurang.r.shah@gmail.com>
2019-05-31 00:01:55 -0400

    working form w/ localstorage -- needs set item

--- src/components/utils/LocalState/LocalState.js
+++ src/components/utils/LocalState/LocalState.js
@@ -1,23 +1,40 @@
 import { useState, useEffect } from 'react';
 
 
-function useLocalStorageState(key, defaultValue) {
+function useLocalStorageState(key, defaultValue, input) {
+  // defaultValue and input are optional, input appies form handling.
   const [state, setState] = useState(() => {
     let value
     try {
-      value = JSON.parse(localStorage.getItem(key) || JSON.stringify(defaultValue))
+      value = JSON.parse(localStorage.getItem(key) || JSON.parse(localStorage.getItem(key)).defaultValue || JSON.stringify(defaultValue))
+      // data in local storage? if not then set it from defaultValue
     } catch (e) {
       value = defaultValue
+      // if any errors, value defaults to defaultValue
     }
     return value
   })
+
   useEffect(
     () => {
       localStorage.setItem(key, state)
     },
     [key, state]
   )
-  return [state, setState]
+  if (input) {
+    const formHandler = {
+      reset: (e) => e.target.value = "",
+      bind: {
+        state,
+        onChange: e => setState(e.target.value)
+      }
+    }
+    return { state, setState, formHandler };
+  } else {
+    return [state, setState,]
+  }
+
+
 }
 
 
